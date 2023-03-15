a9f55c530c7856c7c75eef4f514476d43ee9f5c5
Khajit <khajit1999@gmail.com>
2023-03-14 17:42:16 -0700

    added admin page

--- lib/Pages/PlayerView.dart
+++ lib/Pages/PlayerView.dart
@@ -1,6 +1,7 @@
+
 import 'package:flutter/material.dart';
-
-
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_slider/flutter_slider.dart';
 
 
 class PlayPage extends StatefulWidget {
